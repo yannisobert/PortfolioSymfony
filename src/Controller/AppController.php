15c3ae4e0253b0a5668e4b6feb424402756fd88b
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-04-14 19:14:50 +0200

    :tada: Add first pages

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppController extends AbstractController
 {
-    #[Route('/app', name: 'app_app')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
+
         return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
+            'projectRepo' => $projectRepo,
+        ]);
+    }
+
+    #[Route('/portfolio', name: 'portfolio')]
+    public function portfolio(ProjectRepository $projectRepository): Response
+    {
+        $projectRepo = $projectRepository->findAll();
+
+        return $this->render('app/portfolio.html.twig', [
+            'projectRepo' => $projectRepo,
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Langage;
+use App\Entity\Project;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,26 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 1; $i<=10; $i++) {
+            $project = new Project();
+
+            $project->setName('project ' . $i)
+                ->setTitle('project ' . $i)
+                ->setText('Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum.')
+                ->setGit('https://fdfdgffq.com')
+                ->setImage('test.png')
+                ->setImageHover('test.png');
+
+            $manager->persist($project);
+        }
+
+        for($j = 1; $j<=10; $j++) {
+            $langage = new Langage();
+
+            $langage->setName('langage ' . $j);
+
+            $manager->persist($langage);
+        }
 
         $manager->flush();
     }
