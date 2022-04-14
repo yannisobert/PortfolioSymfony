6936bdb66f30c9acc0636e80b8a6a5cdc6320fe9
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-04-14 18:55:04 +0200

    :tada: Setup project and entity

---
+++ src/Entity/Project.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $imageHover;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $text;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $git;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getImageHover(): ?string
+    {
+        return $this->imageHover;
+    }
+
+    public function setImageHover(string $imageHover): self
+    {
+        $this->imageHover = $imageHover;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getGit(): ?string
+    {
+        return $this->git;
+    }
+
+    public function setGit(?string $git): self
+    {
+        $this->git = $git;
+
+        return $this;
+    }
+}
