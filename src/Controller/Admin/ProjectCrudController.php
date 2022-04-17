2c4f4efbc6eba4d1c76b4f66b8c74539a80ba9cf
yannisobert <90550556+yannisobert@users.noreply.github.com>
2022-04-17 11:57:46 +0200

    :hammer: Update admin part

--- src/Controller/Admin/ProjectCrudController.php
+++ src/Controller/Admin/ProjectCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Project;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProjectCrudController extends AbstractCrudController
 {
@@ -12,14 +16,34 @@ class ProjectCrudController extends AbstractCrudController
         return Project::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')->setDisabled(),
             TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('name'),
+            TextEditorField::new('text'),
+            TextEditorField::new('git'),
+            ImageField::new('image')
+                ->setUploadDir('public/uploads/image')
+                ->setBasePath('uploads/product')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setFormTypeOptions([
+                    'attr' => [
+                        'accept' => 'image/png, image/jpeg'
+                    ]
+                ]),
+            ImageField::new('image_hover')
+                ->setUploadDir('public/uploads/hover')
+                ->setBasePath('uploads/product')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setFormTypeOptions([
+                    'attr' => [
+                        'accept' => 'image/png, image/jpeg'
+                    ]
+                ]),
         ];
     }
-    */
+
 }
